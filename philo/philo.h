d2e186d077f2b30236bddda6b13ce15489361290
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-02-21 17:51:45 +0100

    structs for philosopher and arguments

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 10:20:07 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/02/15 10:21:58 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/02/21 17:32:53 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,37 @@
 # include <stdio.h>
 # include <pthread.h>
 # include <unistd.h>
+# include <limits.h>
+# include <sys/time.h>
+
+typedef struct s_philo
+{
+	pthread_t	thread;
+	t_args		args;
+	int			id;
+	int			meals;
+	int			fork_left;
+	int			fork_right;
+}				t_philo;
+
+typedef struct s_args
+{
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+	int				p_num;
+	int				d_time;
+	int				e_time;
+	int				s_time;
+	int				e_num;
+}					t_args;
+
+/* atoi */
+long	ft_atoi(const char *str);
+
+/* init */
+int		check_input(char **argv);
+t_args	*init_args(int argc, char **argv);
+int		init_philos(t_args *args);
+int		create_philos(t_args *args);
 
 #endif
