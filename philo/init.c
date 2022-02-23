d5029abec0e577351bda460a666c35cdea3a4f38
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-02-23 11:17:31 +0100

    added mutexes

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:15:26 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/02/21 17:44:58 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/02/23 10:48:00 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ t_args	*init_args(int argc, char **argv)
 	args->d_time = ft_atoi(argv[2]);
 	args->e_time = ft_atoi(argv[3]);
 	args->s_time = ft_atoi(argv[4]);
-	if (args->p_num <= 1 || args->p_num > 200)
+	if (args->p_num <= 1)
 		return (0);
 	if (argc == 6)
 	{
@@ -67,14 +67,16 @@ int	init_philos(t_args *args)
 	{
 		args->philos[count].id = count;
 		args->philos[count].meals = 0;
-		args->philos[count].args = args;
 		args->philos[count].fork_left = count;
 		if ((count + 1) < args->p_num)
 			args->philos[count].fork_right = count + 1;
 		else
 			args->philos[count].fork_right = 0;
+		args->philos[count].args = args;
 		count++;
 	}
+	if (!init_mutex(args))
+		return (0);
 	return (1);
 }
 
@@ -92,16 +94,11 @@ int	init_mutex(t_args *args)
 			return (0);
 		count++;
 	}
+	if (pthread_mutex_init(&args->m_is_alive, NULL))
+		return (0);
+	if (pthread_mutex_init(&args->m_eat, NULL))
+		return (0);
 	return (1);
-}
-
-void	*exec_philos(void *philo_c)
-{
-	t_philo	*philo;
-	t_args	*args;
-
-	philo = (t_philo *)philo_c;
-	args = philo->args;
 }
 
 int	create_philos(t_args *args)
@@ -111,18 +108,17 @@ int	create_philos(t_args *args)
 	if (!init_philos(args))
 		return (0);
 	count = 0;
+	args->is_alive = 1;
+	args->start = gettime();
 	while (count < args->p_num)
 	{
 		pthread_create(&(args->philos[count].thread),
 			NULL, exec_philos, &(args->philos[count]));
+		args->until_dead = args->start + args->d_time;
+		//usleep(20);
 		count++;
 	}
-	count = 0;
-	while (count < args->p_num)
-	{
-		pthread_join(args->philos[count].thread, NULL);
-		count++;
-	}
+	checker(args->philos);
 	free(args->forks);
 	free(args->philos);
 	return (1);
