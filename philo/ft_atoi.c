bc4a2a2aca6eb0faa050461460945ad372cab6d7
Ben Tenzlinger <btenzlin@3-C-3.42heilbronn.de>
2022-02-23 11:16:53 +0100

    only pos ints

--- philo/ft_atoi.c
+++ philo/ft_atoi.c
@@ -6,11 +6,23 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 10:46:41 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/02/18 10:33:16 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/02/22 13:31:02 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
 
 static int	ft_isdigit(int a)
 {
@@ -29,20 +41,19 @@ long	ft_atoi(const char *str)
 	count = 0;
 	result = 0;
 	isnegative = 1;
+	if (!str || ft_strlen(str) == 0)
+		return (-1);
 	while (str[count] == '\n' || str[count] == '\v' || str[count] == '\t'
 		|| str[count] == '\r' || str[count] == '\f' || str[count] == ' ')
 		count++;
 	if (str[count] == '-')
-	{
-		count++;
-		isnegative = -1;
-	}	
+		return (-1);
 	else if (str[count] == '+')
 		count++;
 	while (str[count] != '\0')
 	{
 		if (!ft_isdigit(str[count]))
-			return (0);
+			return (-1);
 		result = result * 10 + (str[count] - '0');
 		count++;
 	}
