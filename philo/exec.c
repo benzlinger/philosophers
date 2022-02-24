5a950940b34f9a38211e4ec501d88cad6ef4c94b
Ben Tenzlinger <btenzlin@2-B-7.42heilbronn.de>
2022-02-24 16:10:30 +0100

    remove eat mutexes + add 1 philosopher exception

--- philo/exec.c
+++ philo/exec.c
@@ -6,46 +6,42 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 11:12:32 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/02/23 11:10:17 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/02/24 14:52:01 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	wait_for_action(long time)
+void	wait_for_action(long time, t_args *args)
 {
 	long	start;
 
 	start = gettime();
-	while (gettime() < start + time)
+	while ((gettime() < start + time) && args->is_alive)
 		usleep(500);
 	return ;
 }
 
 void	eat(t_philo *philo)
 {
-	pthread_mutex_lock(&(philo->args->forks[philo->fork_left]));
-	philo_action(philo->id, "has taken a fork\n", philo);
-	pthread_mutex_lock(&(philo->args->forks[philo->fork_right]));
-	philo_action(philo->id, "has taken a fork\n", philo);
-	//philo->args->until_dead = gettime() + gettime();
-	pthread_mutex_lock(&(philo->args->m_eat));
-	philo_action(philo->id, "is eating\n", philo);
-	philo->args->until_dead = gettime() + philo->args->d_time;
-	pthread_mutex_unlock(&(philo->args->m_eat));
-	wait_for_action(philo->args->e_time);
-	//printf("philo: %d\n%ld\n%ld\n\n", philo->id, gettime(), philo->args->until_dead);
+	pthread_mutex_lock(&philo->args->forks[philo->fork_left]);
+	philo_action(philo->id, "has taken a fork", philo);
+	pthread_mutex_lock(&philo->args->forks[philo->fork_right]);
+	philo_action(philo->id, "has taken a fork", philo);
+	philo_action(philo->id, "is eating", philo);
+	philo->last_eat = gettime();
+	wait_for_action(philo->args->e_time, philo->args);
 	philo->meals++;
-	pthread_mutex_unlock(&(philo->args->forks[philo->fork_left]));
-	pthread_mutex_unlock(&(philo->args->forks[philo->fork_right]));
+	pthread_mutex_unlock(&philo->args->forks[philo->fork_left]);
+	pthread_mutex_unlock(&philo->args->forks[philo->fork_right]);
 }
 
 void	my_sleep(t_philo *philo)
 {
 	if (philo->args->is_alive)
 	{
-		philo_action(philo->id, "is sleeping\n", philo);
-		wait_for_action(philo->args->s_time);
+		philo_action(philo->id, "is sleeping", philo);
+		wait_for_action(philo->args->s_time, philo->args);
 	}
 }
 
@@ -54,13 +50,15 @@ void	*exec_philos(void *philo_c)
 	t_philo	*philo;
 
 	philo = (t_philo *)philo_c;
+	if (philo->args->p_num == 1)
+		return (0);
 	if (philo->id % 2)
-		usleep(1000);
+		usleep(philo->args->e_time * 500);
 	while (philo->args->is_alive)
 	{
 		eat(philo);
 		my_sleep(philo);
-		philo_action(philo->id, "is thinking\n", philo);
+		philo_action(philo->id, "is thinking", philo);
 	}
 	return (0);
 }
